56f0fbd5bbc7540155990a60763514b766999596
mertefecerit <mertefecerit@gmail.com>
2024-07-06 17:26:25 +0300

    v1.0.138

--- app/components/BuiComponentShowcaseArea/index.jsx
+++ app/components/BuiComponentShowcaseArea/index.jsx
@@ -2,19 +2,24 @@ import scss from "./BuiComponentShowcaseArea.module.scss";
 import PropTypes from "prop-types";
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import React from "react";
+import {useState} from "react";
 
 function BuiComponentShowcaseArea({children, title, desc, codeBlock, anchor}) {
+    const [clicked,setClicked] = useState(false);
     const copyPath = () => {
         const fullPath = window.location.origin + window.location.pathname + '#' + anchor;
         navigator.clipboard.writeText(fullPath);
+        setClicked(true);
+        setTimeout(() => {
+            setClicked(false)
+        },500)
     }
     return (
         <section id={anchor} className={scss.buiComponentShowcaseArea}>
             <div className={scss.header}>
                 <h2 onClick={() => copyPath()}>
                     {title}
-                    <span> #</span>
+                    <span> # {clicked &&  ' Copied'}</span>
                 </h2>
                 <p>
                     {desc}
