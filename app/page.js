a844f54e5b5a61c091832269aa434f6555b903a1
mertefecerit <mertefecerit@gmail.com>
2024-07-15 01:24:02 +0300

    v1.0.556

--- app/page.js
+++ app/page.js
@@ -1,5 +1,17 @@
+import BuiLogo from "@/app/doc/components/global/BuiLogo";
+import Link from "next/link";
+import {Icon} from "@iconify/react";
+
 export default function HomePage() {
-  return (
-      <div>Main Page</div>
-  )
+    return (
+        <div className="h-dvh flex flex-col gap-8 items-center justify-center">
+            <BuiLogo/>
+            <nav className="font-bold text-xl">
+                <Link className="inline-flex items-center gap-2 hover:text-blue-500 transition-all" href="/doc/getting-started/installation">
+                    <Icon icon="mdi:cursor-default-click" width={25}></Icon>
+                    <span>Click for Documents</span>
+                </Link>
+            </nav>
+        </div>
+    )
 }
