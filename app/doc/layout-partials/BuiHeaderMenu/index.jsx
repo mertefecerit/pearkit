b8bfa6c51b634859f6021849c96f0643ad3d4ef7
mertefecerit <mertefecerit@gmail.com>
2024-07-16 12:48:35 +0300

    v1.0.581

--- app/doc/layout-partials/BuiHeaderMenu/index.jsx
+++ app/doc/layout-partials/BuiHeaderMenu/index.jsx
@@ -4,13 +4,13 @@ import BuiButton from "@/app/components/src/BuiButton";
 function BuiHeaderMenu({mobileSidebarToggle}) {
     return (
         <div className="flex gap-2 items-center">
-            <BuiButton classes="!hidden lg:!flex" variant="outlined" startIcon={<Icon icon="mdi:search"/>}>
+            <BuiButton className="hidden lg:flex" variant="outlined" startIcon={<Icon icon="mdi:search"/>}>
                 Search
             </BuiButton>
-            <BuiButton classes="!flex lg:!hidden" variant="outlined" startIcon={<Icon icon="mdi:search"/>}></BuiButton>
+            <BuiButton className="flex lg:hidden" variant="outlined" startIcon={<Icon icon="mdi:search"/>}></BuiButton>
             <BuiButton href="https://github.com/mertefecerit/bubbleui" variant="outlined" startIcon={<Icon icon="mdi:github"/>}></BuiButton>
             <BuiButton variant="outlined" startIcon={<Icon icon="mdi:cog"/>}></BuiButton>
-            <BuiButton classes="!flex lg:!hidden" color="gray" onClick={mobileSidebarToggle} variant="outlined" startIcon={<Icon icon="mdi:menu"/>}></BuiButton>
+            <BuiButton className="flex lg:hidden" color="gray" onClick={mobileSidebarToggle} variant="outlined" startIcon={<Icon icon="mdi:menu"/>}></BuiButton>
         </div>
     );
 }
