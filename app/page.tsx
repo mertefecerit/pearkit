9baa8d8b5f8bb129c2c0c5761d331555c4db6bc3
mertefecerit <mertefecerit@gmail.com>
2024-07-31 13:26:48 +0300

    v3.0.220

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,33 @@
 import PLogo from "@/app/doc/components/global/PLogo";
-import Link from "next/link";
 import {Icon} from "@iconify/react";
+import {PButton} from "@/app/components/src";
+import React from "react";
 
 export default function HomePage() {
     return (
         <div className="h-dvh flex flex-col gap-8 items-center justify-center dark:bg-gray-900">
             <PLogo/>
-            <nav className="font-bold text-xl">
-                <Link className="inline-flex items-center gap-2 hover:text-lime-500 transition-all" href="/doc/getting-started/installation">
-                    <Icon icon="mdi:cursor-default-click" width={25}></Icon>
-                    <span>Click for Documents</span>
-                </Link>
-            </nav>
+            <div className="flex gap-2">
+                <PButton
+                    startIcon={<Icon icon="mdi:github" width={25}/>}
+                    href={"https://github.com/mertefecerit/pearkit"}
+                    variant={"outlined"}
+                    label={"github"}
+                />
+                <PButton
+                    startIcon={<Icon icon="mdi:npm" width={25}/>}
+                    href={"https://www.npmjs.com/package/pearkit"}
+                    variant={"outlined"}
+                    label={"npm"}
+                />
+                <PButton
+                    startIcon={<Icon icon="mdi:file-document-box-search-outline" width={25}/>}
+                    href={"/doc/getting-started/installation"}
+                    target={"_self"}
+                    variant={"outlined"}
+                    label={"docs"}
+                />
+            </div>
         </div>
     )
 }
